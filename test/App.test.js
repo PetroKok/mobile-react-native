c2de2abe2b06d123536550e515c6a24872d920ed
Petro <kokpetro15@gmail.com>
2019-03-12 00:42:14 +0200

    v1.0

--- test/App.test.js
+++ test/App.test.js
@@ -3,7 +3,7 @@ var assert = require('assert');
 describe('Test array', function () {
     describe('#indexOf()', function () {
         it('should return -1 when the value is not in array', function () {
-            assert.equal([1, 2, 3].indexOf(5), -1);
+            assert.equal([1, 2, 3].indexOf(3), -1);
         });
     });
 });
